33f0ddeca8b66e70af998e072df3f6b4ef7fbd8a
x06lan <x065p4mp6@gmail.com>
2022-02-10 00:06:11 +0800

    init

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./component/three_block"
+// import "./component/three_block"
 
 ReactDOM.render(
     <App />
