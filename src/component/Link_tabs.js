9463e6faeb6a675b831ebdfee4434530d9cf357b
x06lan <x065p4mp6@gmail.com>
2022-02-11 02:36:41 +0800

    fix tab

--- src/component/Link_tabs.js
+++ src/component/Link_tabs.js
@@ -5,7 +5,12 @@ import 'react-tabs/style/react-tabs.css';
 class Link_block extends React.Component{
   render(){
     return(
-      <h3></h3>
+      <a href={this.props.data.href}>
+		    <img src={this.props.data.icon}/>
+        <h4>
+          {this.props.data.name}
+        </h4>
+      </a>
 
     )
   }
@@ -21,7 +26,7 @@ class Link_list extends React.Component{
       <div>
         {
           links.map((item,index)=>{
-            return <Link_block></Link_block>
+            return <Link_block  key={index} data={item}></Link_block>
           })
         }
       </div>
@@ -32,18 +37,21 @@ class Link_tabs extends React.Component{
   render(){
     console.log(this.props.cat)
     console.log(this.props.data)
+      let data=this.props.data
+      let cat=this.props.cat
       return (
       <Tabs>
         <TabList>
-          {this.props.cat.cat.map(function(item,index){
+          {cat.map(function(item,index){
+              console.log(item.name,index)
               return <Tab key={index}>{item.name}</Tab>
           })}
           <Tab>Title 2</Tab>
         </TabList>
 
-          {this.props.cat.cat.map(function(item,index){
-              return <TabPanel key={index}>
-                <Link_block key={index} data={data.data} cat={cat.cat}></Link_block>
+          {cat.map(function(item,index){
+              return <TabPanel >
+                <Link_block key={index} data={data} cat={item}></Link_block>
               </TabPanel>
           })}
         <TabPanel>
