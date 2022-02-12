2203d499f6456820efb07c961812fe3d283fa54c
x06lan <x065p4mp6@gmail.com>
2022-02-13 00:10:50 +0800

    normal function

--- src/component/Link_tabs.js
+++ src/component/Link_tabs.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import './Link_tabs.css';
 
 class Link_block extends React.Component{
   render(){
+    // console.log(this.props.data.herf);
     return(
-      <a href={this.props.data.href}>
-		    <img src={this.props.data.icon}/>
-        <h4>
+      <a href={this.props.data.herf} className='link_block'>
+		    <img src={this.props.data.icon} className='link_image'/>
+        <h4 className='link_title'>
           {this.props.data.name}
         </h4>
       </a>
@@ -19,13 +21,18 @@ class Link_list extends React.Component{
   
   render(){
     let links=[]
-    links=this.props.cat.index.map((item,index)=>{
-      return this.props.data[index]
+    links=this.props.cat.indata.map((item,index)=>{
+      return this.props.data[item]
     })
+    console.log(this.props.cat.name);
+    console.log(links);
+    
     return(
-      <div>
+      <div className='container'>
         {
           links.map((item,index)=>{
+            // console.log(item);
+            // return <h3> {index}</h3>
             return <Link_block  key={index} data={item}></Link_block>
           })
         }
@@ -35,29 +42,31 @@ class Link_list extends React.Component{
 }
 class Link_tabs extends React.Component{
   render(){
-    console.log(this.props.cat)
-    console.log(this.props.data)
+    // console.log(this.props.cat)
+    // console.log(this.props.data)
       let data=this.props.data
       let cat=this.props.cat
       return (
-      <Tabs>
-        <TabList>
-          {cat.map(function(item,index){
-              console.log(item.name,index)
-              return <Tab key={index}>{item.name}</Tab>
-          })}
-          <Tab>Title 2</Tab>
-        </TabList>
+        <div>
+          <Tabs>
+            <TabList>
+              {cat.map(function(item,index){
+                  // console.log(item.name,index)
+                  return <Tab key={index}>{item.name}</Tab>
+              })}
+              <Tab>Title 2</Tab>
+            </TabList>
 
-          {cat.map(function(item,index){
-              return <TabPanel >
-                <Link_block key={index} data={data} cat={item}></Link_block>
-              </TabPanel>
-          })}
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-      </Tabs>
+              {cat.map(function(item,index){
+                  return <TabPanel >
+                    <Link_list key={index} data={data} cat={item}></Link_list>
+                  </TabPanel>
+              })}
+            <TabPanel>
+              <h2>Any content 2</h2>
+            </TabPanel>
+          </Tabs>
+        </div>
     )
   }
 }
