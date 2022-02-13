743c820836c6d110b62c9af876994da146921202
x06lan <x065p4mp6@gmail.com>
2022-02-13 23:29:50 +0800

    search bar done

--- src/component/Input_search.js
+++ src/component/Input_search.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './Input_search.css';
 
 class Input_search extends React.Component{
+
     render(){
+        function search(){
+            let text=document.querySelector("#input_search").value
+            // let text=document.querySelector("#input_search").value
+            console.log(text)
+        }
         return(
             <div className='search_box'>
                 <input id='input_search' className='input_search'></input>
-                <button id='btn_search' className='btn_search'>搜索</button>
+                <div className='btn_search'onClick={search}>
+                    <input className='btn_image' type="image" src="./img/search.png"  ></input>
+                </div>
+
             </div>
         )
     }
